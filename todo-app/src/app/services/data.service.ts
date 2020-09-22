3912f4f81e7eb1f2dba5f813197b5b4a052f8c0d
tainguyen2019 <tainguyen.uit@gmail.com>
2020-09-22 16:44:19 +0700

    Add Calling API

---
+++ todo-app/src/app/services/data.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  private apiURL = 'http://localhost:3000';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+  constructor(private httpClient: HttpClient) {}
+
+  getPosts(): Observable<any> {
+    return this.httpClient.get(`${this.apiURL}/posts`);
+  }
+
+  deletePost(id) {
+    return this.httpClient.delete(
+      `${this.apiURL}/posts/${id}`,
+      this.httpOptions
+    );
+  }
+
+  getPost(id): Observable<any> {
+    return this.httpClient.get(`${this.apiURL}/posts/${id}`);
+  }
+
+  updatePost(id, post): Observable<any> {
+    return this.httpClient.put(
+      `${this.apiURL}/posts/${id}`,
+      JSON.stringify(post),
+      this.httpOptions
+    );
+  }
+}
